10fc7c17a940086c019e2a16283c5db82b07ac8d
Viswanath Reddy Marala <viswanathreddy081@gmail.com>
2025-04-30 19:20:27 +0530

    Delete README.c

--- README.c
+++
@@ -1,2 +0,0 @@
-# Medicine-reminder-system
-A Medicine Reminder System using LPC2148, LCD, RTC, and a 4x4 keypad allows users to set medicine schedules, displaying real-time clock info and triggering buzzer alerts when it's time to take medicine. Users acknowledge intake via a switch, ensuring timely medication reminders.
